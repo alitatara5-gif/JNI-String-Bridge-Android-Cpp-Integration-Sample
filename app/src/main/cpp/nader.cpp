6902478f781d3dc6f747ffe1e7a0f1912bf6256a
alitatara5-gif <alitatara5@gmail.com>
2026-04-30 01:47:58 +0800

    Update nader.cpp

--- app/src/main/cpp/nader.cpp
+++ app/src/main/cpp/nader.cpp
@@ -1,8 +1,27 @@
 #include <jni.h>
 #include <string>
 
+// Ini fungsi bawaan Abang sebelumnya (biarkan saja buat tes)
 extern "C" JNIEXPORT jstring JNICALL
 Java_nademkhan_example_jnistringbridge_MainActivity_getNativeString(JNIEnv* env, jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+// =========================================================
+// KABEL BARU: WAJIB DITAMBAH BIAR TOMBOL GAK BIKIN CRASH
+// =========================================================
+
+// Kabel untuk tombol StartBigoEngine
+extern "C" JNIEXPORT void JNICALL
+Java_nademkhan_example_jnistringbridge_MainActivity_StartBigoEngine(JNIEnv* env, jobject thiz, jstring id, jstring ffmpegPath) {
+    // Kosongkan dulu untuk tes nyala. 
+    // Nanti kode untuk manggil header Go (libbigo_engine.h) kita taruh di dalam sini.
+}
+
+// Kabel untuk tombol StopBigoEngine
+extern "C" JNIEXPORT void JNICALL
+Java_nademkhan_example_jnistringbridge_MainActivity_StopBigoEngine(JNIEnv* env, jobject thiz, jstring id) {
+    // Kosongkan dulu untuk tes nyala.
+    // Nanti perintah stop Go ditaruh di sini.
 }
