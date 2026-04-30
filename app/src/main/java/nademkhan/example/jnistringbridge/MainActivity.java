f232b2b2b716e3478c9e6a1338838a1f04ab2bd8
alitatara5-gif <alitatara5@gmail.com>
2026-04-30 13:17:19 +0800

    Update MainActivity.java

--- app/src/main/java/nademkhan/example/jnistringbridge/MainActivity.java
+++ app/src/main/java/nademkhan/example/jnistringbridge/MainActivity.java
@@ -1,6 +1,5 @@
 package nademkhan.example.jnistringbridge;
 
-// --- BAGIAN IMPORT (WAJIB ADA SEMUA) ---
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -20,49 +19,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Meminta izin akses file untuk Android 11 ke atas
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
             Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
             intent.setData(Uri.parse("package:" + getPackageName()));
             startActivity(intent);
         }
 
-        // Setup WebView
+        // Jalankan mesin Go saat aplikasi dibuka
+        Intent engineIntent = new Intent(this, RecorderService.class);
+        engineIntent.setAction("START_ENGINE");
+        startService(engineIntent);
+
         WebView myWebView = findViewById(R.id.webview_dashboard);
         WebSettings ws = myWebView.getSettings();
         ws.setJavaScriptEnabled(true);
         ws.setDomStorageEnabled(true);
         ws.setAllowFileAccess(true);
-        ws.setAllowContentAccess(true);
 
-        // Menghubungkan HTML dengan Java (AndroidBridge)
         myWebView.addJavascriptInterface(new WebAppInterface(), "AndroidBridge");
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("file:///android_asset/index.html");
     }
 
-    // Kelas jembatan untuk JavaScript
     public class WebAppInterface {
-        
         @JavascriptInterface
         public void startRecording(String userID) {
-            // Memanggil Service untuk mulai rekam
             Intent intent = new Intent(MainActivity.this, RecorderService.class);
-            intent.setAction("START");
+            intent.setAction("ADD_ID");
             intent.putExtra("userID", userID);
-            
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(intent);
-            } else {
-                startService(intent);
-            }
+            startService(intent);
         }
 
         @JavascriptInterface
         public void stopRecording(String userID) {
-            // Memanggil Service untuk stop rekam
             Intent intent = new Intent(MainActivity.this, RecorderService.class);
-            intent.setAction("STOP");
+            intent.setAction("STOP_ID");
             intent.putExtra("userID", userID);
             startService(intent);
         }
