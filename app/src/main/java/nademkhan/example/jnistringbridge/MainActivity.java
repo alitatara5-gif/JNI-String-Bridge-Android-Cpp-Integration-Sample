f5b85afb517f1bd0d6ffffe59c5d49329e447044
alitatara5-gif <alitatara5@gmail.com>
2026-04-30 12:35:34 +0800

    Update MainActivity.java

--- app/src/main/java/nademkhan/example/jnistringbridge/MainActivity.java
+++ app/src/main/java/nademkhan/example/jnistringbridge/MainActivity.java
@@ -1,10 +1,42 @@
 package nademkhan.example.jnistringbridge;
 
+// BAGIAN IMPORT INI WAJIB ADA SEMUA!
 import android.content.Intent;
-// ... import lainnya ...
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    // ... isi onCreate ...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Izin Storage
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Environment.isExternalStorageManager()) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        }
+
+        WebView myWebView = findViewById(R.id.webview_dashboard);
+        WebSettings ws = myWebView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setAllowFileAccess(true);
+
+        myWebView.addJavascriptInterface(new WebAppInterface(), "AndroidBridge");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
 
     public class WebAppInterface {
         @JavascriptInterface
@@ -12,15 +44,19 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, RecorderService.class);
             intent.setAction("START");
             intent.putExtra("userID", userID);
-            // ... dst ...
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent);
+            } else {
+                startService(intent);
+            }
         }
 
         @JavascriptInterface
         public void stopRecording(String userID) {
             Intent intent = new Intent(MainActivity.this, RecorderService.class);
             intent.setAction("STOP");
-            // ... dst ...
+            intent.putExtra("userID", userID);
+            startService(intent);
         }
     }
 }
-// JANGAN ADA KELAS LAIN DI LUAR KURUNG KURAWAL INI
